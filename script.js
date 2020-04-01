9973cc2bbf339b60ffbe9c68445befc28a2f8f3f
Jeff King <jazfunk@gmail.com>
2020-04-01 16:02:57 -0400

    User enters Keys P, R, or S).  Generates random CPU, determines winner, alerts user.  No image interations yet.

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-
-var isChoiceMade = false;
+//var isChoiceMade = false;
 var prsChoice = "";
+var toolIDs = ["R", "P", "S"];
+var tools = [];
+
+class Tool {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+}
+
+window.onload = function() {
+  setTools();
+};
+
+function setTools() {
+  for (t in toolIDs) {
+    toolClass = new Tool(toolIDs[t], getToolName(toolIDs[t]));
+    tools.push(toolClass);
+  }
+}
+
+function getToolName(id) {
+  var toolName = "";
+  switch (id) {
+    case "R":
+      toolName = "Rock";
+      break;
+    case "P":
+      toolName = "Paper";
+      break;
+    case "S":
+      toolName = "Scissors";
+      break;
+    case "TIE":
+      toolName = "Tie";
+      break;
+  }
+  return toolName;
+}
 
 document.addEventListener("keyup", keyUpHandler, false);
-
 function keyUpHandler(e) {
   switch (e.which) {
     case 80:
@@ -16,19 +53,23 @@ function keyUpHandler(e) {
       prsChoice = "S";
       break;
   }
-  if(prsChoice != "") {
-      isChoiceMade = true;
-  } else {
-      isChoiceMade = false;
-  }
+  var cpuChoice = cpuRandomPRS(1, "PRS");
 
-  var temp = cpuRandomPRS(1, "PRS");
-  alert(prsChoice + temp);  
-  alert(calculateWinner(prsChoice + temp));
+  alert(`You:  ${getToolName(prsChoice)} -vs- CPU:  ${getToolName(cpuChoice)}`);
+
+  var result = calculateWinner(prsChoice + cpuChoice);
+  var rule = getRule(prsChoice + cpuChoice);  
+  result != "TIE"
+    ? alert(`${getToolName(result)} Wins! -${rule}`)
+    : alert("Tie Game");
 }
 
-
-  
+function cpuRandomPRS(length, chars) {
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
 
 function calculateWinner(opponents) {
   var winner = "NONE";
@@ -54,49 +95,51 @@ function calculateWinner(opponents) {
   return winner;
 }
 
-
-function cpuRandomPRS(length, chars) {
-  var result = "";
-  for (var i = length; i > 0; --i)
-    result += chars[Math.round(Math.random() * (chars.length - 1))];
-  return result;
+function getRule(opponents) {
+  var rule = "NONE";
+  switch (opponents) {
+    case "PR":
+    case "RP":
+      rule = "Paper Covers Rock";
+      break;
+    case "PS":
+    case "SP":
+      rule = "Scissors Cut Paper";
+      break;
+    case "RS":
+    case "SR":
+      rule = "Rock Smashes Scissors";
+      break;
+    case "PP":
+    case "RR":
+    case "SS":
+      rule = "TIE";
+      break;
+  }
+  return rule;
 }
-//document.write(randomString(1, 'PRS'));
-
-
-
-
-
-
 
 document
   .getElementById("page-action-btn")
   .addEventListener("click", function(e) {
+    e.preventDefault;
     var cellTarget = document.getElementById("user1").innerHTML;
     console.log(cellTarget);
     document.getElementById("run-text").value = cellTarget;
-    
     var userPRS = prompt("Paper, Rock, or Scissors?").toUpperCase();
     var battle = userPRS + cpuRandomPRS(1, "PRS");
     console.log(battle);
-    alert("Battle = " + battle);    
-    alert(calculateWinner(battle));
   });
-
 
 document.getElementById("user2").addEventListener("click", function(e) {
   var cell = e.target.parentElement;
   var cellVerticalAlign = cell.style.verticalAlign;
-  debugger;
   if (cellVerticalAlign === "top" || cellVerticalAlign === "") {
-    //cell.style.verticalAlign = "middle";
     cellVerticalAlign = "middle";
   } else {
     if (cellVerticalAlign === "middle") {
-      //cell.style.verticalAlign = "bottom";
       cellVerticalAlign = "bottom";
     } else {
-      //cell.style.verticalAlign = "top";
       cellVerticalAlign = "top";
     }
   }
