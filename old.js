6a7be0da71f18e893ed4c27569ed291c4b4b2601
Jeff King <jazfunk@gmail.com>
2020-04-01 19:59:16 -0400

    Code cleaned up and simplified.  Still no graphic interaction.

---
+++ old.js
@@ -0,0 +1,44 @@
+// const TOOL_IDS = ["R", "P", "S"];
+// var tools = [];
+
+// class Tool {
+//   constructor(id, name) {
+//     this.id = id;
+//     this.name = name;    
+//   }  
+// }
+
+// window.onload = function() {
+// //   setTools();
+// };
+
+// function setTools() {
+//   for (t in TOOL_IDS) {
+//     toolClass = new Tool(TOOL_IDS[t], getToolName(TOOL_IDS[t]));
+//     tools.push(toolClass);
+//   }
+// }
+
+// function getRule(opponents) {
+//   //var rule = "NONE";
+//   switch (opponents) {
+//     case "PR":
+//     case "RP":
+//       rule = "Paper Covers Rock";
+//       break;
+//     case "PS":
+//     case "SP":
+//       rule = "Scissors Cut Paper";
+//       break;
+//     case "RS":
+//     case "SR":
+//       rule = "Rock Smashes Scissors";
+//       break;
+//     case "PP":
+//     case "RR":
+//     case "SS":
+//       rule = "TIE";
+//       break;
+//   }
+//   return rule;
+// }
--- script.js
+++ script.js
@@ -1,25 +1,66 @@
-//var isChoiceMade = false;
-var prsChoice = "";
-var toolIDs = ["R", "P", "S"];
-var tools = [];
+var userChoice = "";
+var cpuChoice = "";
+var rule = "";
 
-class Tool {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyUpHandler(e) {
+  userChoice = "";  
+  switch (e.which) {
+    case 80:
+      userChoice = "P";
+      break;
+    case 82:
+      userChoice = "R";
+      break;
+    case 83:
+      userChoice = "S";
+      break;
+  }  
+  if (userChoice !== "") {
+    playGame();
   }
 }
 
-window.onload = function() {
-  setTools();
-};
+function playGame() {
+  cpuChoice = getRandom(1, "PRS");
+  alert(
+    `You: ${getToolName(userChoice)}  -vs-  CPU: ${getToolName(cpuChoice)}`
+  );
 
-function setTools() {
-  for (t in toolIDs) {
-    toolClass = new Tool(toolIDs[t], getToolName(toolIDs[t]));
-    tools.push(toolClass);
-  }
+  var outcome = calculateWinner(userChoice + cpuChoice);
+  outcome != "TIE"
+    ? alert(`${getToolName(outcome)} Wins! (${rule})`)
+    : alert("Tie Game");
 }
+
+function calculateWinner(opponents) {
+    var winner = "NONE";
+    switch (opponents) {
+      case "PR":
+      case "RP":
+        winner = "P";
+        rule = "Paper Covers Rock"
+        break;
+      case "PS":
+      case "SP":
+        winner = "S";
+        rule = "Scissors Cut Paper";
+        break;
+      case "RS":
+      case "SR":
+        winner = "R";
+        rule = "Rock Smashes Scissors";
+        break;
+      case "PP":
+      case "RR":
+      case "SS":
+        winner = "TIE";
+        rule = winner;
+        break;
+    }
+    return winner;
+  } 
 
 function getToolName(id) {
   var toolName = "";
@@ -40,83 +81,11 @@ function getToolName(id) {
   return toolName;
 }
 
-document.addEventListener("keyup", keyUpHandler, false);
-function keyUpHandler(e) {
-  switch (e.which) {
-    case 80:
-      prsChoice = "P";
-      break;
-    case 82:
-      prsChoice = "R";
-      break;
-    case 83:
-      prsChoice = "S";
-      break;
-  }
-  var cpuChoice = cpuRandomPRS(1, "PRS");
-
-  alert(`You:  ${getToolName(prsChoice)} -vs- CPU:  ${getToolName(cpuChoice)}`);
-
-  var result = calculateWinner(prsChoice + cpuChoice);
-  var rule = getRule(prsChoice + cpuChoice);  
-  result != "TIE"
-    ? alert(`${getToolName(result)} Wins! -${rule}`)
-    : alert("Tie Game");
-}
-
-function cpuRandomPRS(length, chars) {
+function getRandom(length, chars) {
   var result = "";
   for (var i = length; i > 0; --i)
     result += chars[Math.round(Math.random() * (chars.length - 1))];
   return result;
-}
-
-function calculateWinner(opponents) {
-  var winner = "NONE";
-  switch (opponents) {
-    case "PR":
-    case "RP":
-      winner = "P";
-      break;
-    case "PS":
-    case "SP":
-      winner = "S";
-      break;
-    case "RS":
-    case "SR":
-      winner = "R";
-      break;
-    case "PP":
-    case "RR":
-    case "SS":
-      winner = "TIE";
-      break;
-  }
-  return winner;
-}
-
-function getRule(opponents) {
-  var rule = "NONE";
-  switch (opponents) {
-    case "PR":
-    case "RP":
-      rule = "Paper Covers Rock";
-      break;
-    case "PS":
-    case "SP":
-      rule = "Scissors Cut Paper";
-      break;
-    case "RS":
-    case "SR":
-      rule = "Rock Smashes Scissors";
-      break;
-    case "PP":
-    case "RR":
-    case "SS":
-      rule = "TIE";
-      break;
-  }
-  return rule;
 }
 
 document
@@ -124,24 +93,23 @@ document
   .addEventListener("click", function(e) {
     e.preventDefault;
     var cellTarget = document.getElementById("user1").innerHTML;
-    console.log(cellTarget);
     document.getElementById("run-text").value = cellTarget;
-    var userPRS = prompt("Paper, Rock, or Scissors?").toUpperCase();
-    var battle = userPRS + cpuRandomPRS(1, "PRS");
-    console.log(battle);
   });
 
-document.getElementById("user2").addEventListener("click", function(e) {
-  var cell = e.target.parentElement;
-  var cellVerticalAlign = cell.style.verticalAlign;
-  if (cellVerticalAlign === "top" || cellVerticalAlign === "") {
-    cellVerticalAlign = "middle";
-  } else {
-    if (cellVerticalAlign === "middle") {
-      cellVerticalAlign = "bottom";
+document
+  .getElementById("user2")
+  .addEventListener("click", function(e) {
+    e.preventDefault;
+    var cell = e.target.parentElement;
+    var cellVerticalAlign = cell.style.verticalAlign;
+    if (cellVerticalAlign === "top" || cellVerticalAlign === "") {
+      cellVerticalAlign = "middle";
     } else {
-      cellVerticalAlign = "top";
+      if (cellVerticalAlign === "middle") {
+        cellVerticalAlign = "bottom";
+      } else {
+        cellVerticalAlign = "top";
+      }
     }
-  }
   cell.style.verticalAlign = cellVerticalAlign;
-});
+  });
